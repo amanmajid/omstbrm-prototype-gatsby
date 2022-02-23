13950fc74a27a871ee10bb917e75a914a42f2f4a
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2022-02-23 16:56:35 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,6 @@ export const pageQuery = graphql`
           id
           excerpt(pruneLength: 250)
           frontmatter {
-            // date(formatString: "MMMM DD, YYYY")
             path
             title
             thumbnail
